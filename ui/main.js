85619d52f402da2f2ea4b32ec1fb057ff0904785
Muthukumar2396 <muthukumar.ndr@gmail.com>
2017-02-27 22:24:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('modi');
+var marginleft = 0;
+function moveRight(){
+    marginleft = marginleft+10;
+    img.style.marginleft= marginleft + 'px';
+}
 img.onclick = function(){
+    var interval = setInterval(moveRight,100);
 img.style.marginleft = '100px';
 };
