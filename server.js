4f8107178ceb380f9d4e240daada415eac68f25f
Muthukumar2396 <muthukumar.ndr@gmail.com>
2017-02-27 20:26:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/article-one', function(rq,res){
+   res.send('Article one requested and will be served here'); 
+});
+app.get('/article-two', function(rq,res){
+   res.send('Article one requested and will be served here'); 
+});
+app.get('/article-three', function(rq,res){
+   res.send('Article one requested and will be served here'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
