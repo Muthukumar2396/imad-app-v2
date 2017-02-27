8c484cf27c956f2251ef58c35abaf92b955756b6
Muthukumar2396 <muthukumar.ndr@gmail.com>
2017-02-28 00:29:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,14 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New Value';
-
-//move image
-
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft = marginLeft+5;
-    img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+//copunter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick= function(){
+  // make a req to the counter
+  
+  // capture the res and store it in a variable
+  
+  
+  //Render the variable in the correct span
+  counter=counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
