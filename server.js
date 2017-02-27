6ae277502da79c0939fc439e7eba6a10811bb88c
Muthukumar2396 <muthukumar.ndr@gmail.com>
2017-02-28 01:23:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/ui/main.js', function (req, res) {
 
 var names = [];
 app.get('/submit-name/:name', function (req, res) {
- var name = req.param.name;
+ var name = req.params.name;
  
  names.push(name);
  
